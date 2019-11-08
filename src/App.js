a37116eb2be1886443c25e2d06a1837c2c420133
Sylvain COMELLI <sylvain.comelli@gmail.com>
2019-11-08 13:03:15 +0100

    todo list input works - need to add display feature

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [taskInput, setTaskInput] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  return (
+    <>
+      <h1>To-Do list</h1>
+
+      <div className="form">
+        <form
+          onSubmit={event => {
+            event.preventDefault();
+            const newTask = [...todoList, taskInput];
+            setTodoList(newTask);
+            setTaskInput("");
+          }}
+        >
+          <input
+            className="task-input"
+            type="text"
+            value={taskInput}
+            onChange={event => setTaskInput(event.target.value)}
+          />
+          <button>Add task</button>
+        </form>
+        {console.log(todoList)}
+      </div>
+    </>
+  );
 }
 
 export default App;
